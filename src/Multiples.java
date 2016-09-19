cfa20002733020ac6ab9fcb92c1067dc749a35a2
NehaJamwal <nehajamwal89@gmail.com>
2016-09-19 13:58:39 -0400

    commit

---
+++ src/Multiples.java
@@ -0,0 +1,33 @@
+
+
+
+public class Multiples {
+	public static void main(String[] args) {
+		
+		
+		//Scanner number = new Scanner(System.in);
+		
+		//System.out.println("Enter a number \n");
+		//x= number.nextInt();
+		
+		for (int i=1;i<=100; i++)
+		
+			if (i%15 == 0)
+					System.out.println("FIZZBUZZ");
+			else if (i%5 == 0)
+					System.out.println("BUZZ");
+			else if (i%3 == 0 )
+					System.out.println("FIZZ");
+			else
+				System.out.println(i);;
+			
+			
+		}
+	
+			
+			
+		
+		
+	
+	}
+
